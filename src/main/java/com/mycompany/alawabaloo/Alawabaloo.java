c63136dfa25d6eba342928c09b3254f82090f633
roycebalverde <roycebalverde@gmail.com>
2024-04-22 19:41:35 +0800

    first commit

---
+++ src/main/java/com/mycompany/alawabaloo/Alawabaloo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.alawabaloo;
+
+/**
+ *
+ * @author User
+ */
+public class Alawabaloo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
